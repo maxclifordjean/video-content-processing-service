e40ea0ad26fa635a8d45291da3c4f2d89d9ff5c5
HAMIDOUCHE Lyes <lyes.hamidouche>
2023-07-28 10:30:33 +0200

    HLS reader done:  merge ongoing

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ app = web.Application([
 ])
 
 if __name__ == "__main__":
-    define("port", default=os.getenv("PORT", 5000), help="port", type=int)
-    define("ip", default=os.getenv("HOST", "127.0.0.1"), help="host")
+    define("port", default=os.getenv("PORT", 8000), help="port", type=int)
+    define("ip", default=os.getenv("HOST", "localhost"), help="host")
     parse_command_line()
     print("[VIDEO-CONTENT-PROCESSING-MS]: Server running: " + options.ip + ":" + str(options.port))
     app.listen(options.port, address=options.ip)
